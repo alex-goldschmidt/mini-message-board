95d45de1d72eb23304f0395737534f04839f4bd9
alex-goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2023-02-01 15:04:21 -0500

    Show messages on local server

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Express", messages: messages });
+});
+router.get("/new", function (req, res, next) {
+  res.render("form");
 });
 
 module.exports = router;
